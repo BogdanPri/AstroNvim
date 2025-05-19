1bb5d763821636122c4f07a2ecb64da6f86b3de4
BogdanPri <bogdan_pricope@mailbox.org>
2025-05-19 12:01:18 +0100

    Modified `nvim/lua/user/options.lua` to check the OS before setting the shell

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,5 +1,10 @@
--- Use fish as the shell:
-vim.opt.shell = "/usr/bin/fish"
-
--- Use pwsh as the shell:
--- vim.opt.shell = "pwsh"
+if vim.fn.has("win32") == 1 then
+  -- On Windows
+  vim.opt.shell = "pwsh"
+  vim.opt.shellcmdflag = "-NoLogo -NoProfile -Command"
+  vim.opt.shellquote = ""
+  vim.opt.shellxquote = ""
+else
+  -- On Linux/macOS (or anything else)
+  vim.opt.shell = "/usr/bin/fish"  -- or the output of `which fish`
+end
