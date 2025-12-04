cde4fd500081d748f86b43203f379bfefef420e1
Bogdan Pricope <bogdan.pricope6198@gmail.com>
2025-12-04 11:19:43 +0000

    Updated mappings.lua

--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -5,14 +5,15 @@ return {
     mappings = {
       n = {
         ["<Leader>G"] =  { desc=" GitHub Copilot" },
-        ["<Leader>Ge"] = { "<cmd>Copilot enable<cr>", desc="Enable Copilot" },
+        ["<Leader>Ge"] = { "<cmd>Copilot enable<cr>",  desc="Enable Copilot" },
         ["<Leader>Gd"] = { "<cmd>Copilot disable<cr>", desc="Disable Copilot" },
-        ["<Leader>Gh"] = { "<cmd>Copilot help<cr>", desc="Copilot Help Menu" },
-        ["<Leader>Ga"] = { "<cmd>Copilot auth<cr>", desc="Copilot Auth" },
-        ["<Leader>Gt"] = { "<cmd>CopilotToggle<cr>", desc="FloatTerm copilot"  },
+        ["<Leader>Gh"] = { "<cmd>Copilot help<cr>",    desc="Copilot Help Menu" },
+        ["<Leader>Ga"] = { "<cmd>Copilot auth<cr>",    desc="Copilot Auth" },
+        ["<Leader>Gt"] = { "<cmd>CopilotToggle<cr>",   desc="FloatTerm copilot"  },
+        ["<Leader>Gc"] = { "<cmd>CopilotChat<cr>",     desc="FloatTerm copilot"  },
         ["<Leader>m"] =  { desc="Neominimap" },
-        ["<Leader>me"] = { "<cmd>Neominimap Enable<cr>", desc="Enable minimap" },
-        ["<Leader>md"] = { "<cmd>Neominimap Disable<cr>", desc="Disable minimap" },
+        ["<Leader>me"] = { "<cmd>Neominimap Enable<cr>",    desc="Enable minimap" },
+        ["<Leader>md"] = { "<cmd>Neominimap Disable<cr>",   desc="Disable minimap" },
         ["<Leader>mt"] = { "<cmd>Neominimap BufToggle<cr>", desc="Toggle minimap" },
       },
       t = {
